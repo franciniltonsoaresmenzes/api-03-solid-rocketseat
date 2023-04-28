8a8d713b55429ee21a6b9dc2e3600acd56f2382d
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-27 21:34:16 -0300

    Configurando ESLint

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,12 +3,12 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
+if (_env.success === false) {
   console.error('Invalid enviroment variables', _env.error.format())
 
   throw new Error('Invalid enviroment variables')
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => console.log('HTTP Server Runnig!'))
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => console.log('HTTP Server Runnig!'))
