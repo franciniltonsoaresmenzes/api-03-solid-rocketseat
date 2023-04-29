d2cfaaaa120a7683df29a90d217f2df604b1da01
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-29 20:27:48 -0300

    PostgreSQL com Docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+  },
+})
