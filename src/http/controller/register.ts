b4ceada4dcfd595a8fa340eedd5f61a562ee8bdb
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-29 23:06:34 -0300

    register

---
+++ src/http/controller/register.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  await prisma.user.create({ data: { name, email, password_hash } })
+
+  return reply.status(201).send()
+}
