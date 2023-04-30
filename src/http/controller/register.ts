ff22242aee6135ee4f9ca07d0e54ec5bca9f8980
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-30 20:41:28 -0300

    Caso de uso de registro

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,5 +1,4 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+import { registerUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -12,9 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  await prisma.user.create({ data: { name, email, password_hash } })
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
+    return reply.status(401).send()
+  }
 
   return reply.status(201).send()
 }
