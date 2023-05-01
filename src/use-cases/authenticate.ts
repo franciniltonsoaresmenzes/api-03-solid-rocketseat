358d8e946f3011f5ef3ffc1e4f6aed0017356e73
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-01 17:06:41 -0300

    Caso de uso de autenticacao

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsErros } from './erros/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class Authenticate {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsErros()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsErros()
+    }
+
+    return { user }
+  }
+}
---
+++ src/use-cases/erros/invalid-credentials-error.ts
@@ -0,0 +1,5 @@
+export class InvalidCredentialsErros extends Error {
+  constructor() {
+    super('Invalid credentials.')
+  }
+}
