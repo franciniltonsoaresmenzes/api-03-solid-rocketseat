81ea5999695fd8dfe46346b57ae88fdf75f405c1
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-06 18:43:21 -0300

    Caso de uso de metricas

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -31,6 +31,10 @@ export class InMemoryCheckInsRepository implements CheckInRepository {
       .slice((page - 1) * 20, page * 20)
   }
 
+  async countByUserId(userId: string): Promise<number> {
+    return this.items.filter((item) => item.user_id === userId).length
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
---
+++ src/use-cases/get-user-metrics.ts
@@ -0,0 +1,23 @@
+import { CheckInRepository } from '@/repositories/checkin-repository'
+
+interface GetUserMetricsUseCaseRequest {
+  userId: string
+}
+
+interface GetUserMetricsUseCaseResponse {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private chekInRepository: CheckInRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkInsCount = await this.chekInRepository.countByUserId(userId)
+
+    return {
+      checkInsCount,
+    }
+  }
+}
