4779b6f5fdf7de041ada3167f105b538dcc4f4e0
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-06 17:28:21 -0300

    Validando distancia do check-in

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import { CheckIn } from '@prisma/client'
 import { CheckInRepository } from '@/repositories/checkin-repository'
 import { GymsRepository } from '@/repositories/gyms-repository'
 import { ResourceNotFound } from './erros/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -23,12 +24,29 @@ export class CheckInUseCase {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId)
 
     if (!gym) {
       throw new ResourceNotFound()
     }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error()
+    }
+
     const checkInOnSameDay = await this.chekInRepository.findByUserIdOnDate(
       userId,
       new Date(),
