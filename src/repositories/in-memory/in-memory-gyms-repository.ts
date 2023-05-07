09f92e1ccaf4b3e9755b05c2ef4b8677330ec064
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-06 21:23:00 -0300

    Caso de uso de busca de academias

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -15,6 +15,12 @@ export class InMemoryGymsRepositories implements GymsRepository {
     return user
   }
 
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    return this.items
+      .filter((item) => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20)
+  }
+
   async create(data: Prisma.GymCreateInput) {
     const gym: Gym = {
       id: data.id ?? randomUUID(),
