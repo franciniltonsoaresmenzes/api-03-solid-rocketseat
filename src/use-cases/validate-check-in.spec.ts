ca271a8f66cd112d3cff9dd16efe76214616fd3f
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-06 22:26:00 -0300

    Validando horario do check-in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { ResourceNotFound } from './erros/resource-not-found-error'
+import { LateCheckInValidateError } from './erros/late-check-in-validate-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
@@ -11,11 +12,11 @@ describe('Validate Check-in Use Case', () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check-in', async () => {
@@ -36,5 +37,24 @@ describe('Validate Check-in Use Case', () => {
     await expect(() =>
       sut.execute({ checkInId: 'inexist' }),
     ).rejects.toBeInstanceOf(ResourceNotFound)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidateError)
   })
 })
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from '@prisma/client'
 import { CheckInRepository } from '@/repositories/checkin-repository'
 import { ResourceNotFound } from './erros/resource-not-found-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidateError } from './erros/late-check-in-validate-error'
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -20,6 +22,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFound()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidateError()
     }
 
     checkIn.validated_at = new Date()
