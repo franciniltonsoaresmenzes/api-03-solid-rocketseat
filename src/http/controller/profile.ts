dde5a35a9f8d39168136bf2804f1a7dbb53afba1
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-10 20:51:08 -0300

    Implementando JWT no Fastify

---
+++ src/http/controller/profile.ts
@@ -0,0 +1,7 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify()
+  console.log(request.user.sub)
+  return reply.status(200).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controller/register'
 import { authenticate } from './controller/authenticate'
+import { profile } from './controller/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticate
+  app.get('/me', profile)
 }
